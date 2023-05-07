c24b4467879374daeba523a021a244394c6e6be1
memeToasty <memeToasty@gmail.com>
2023-05-07 12:53:57 +0200

    Added testing with travis

---
+++ backend/services/register/handler/register_test.go
@@ -0,0 +1,21 @@
+package handler_test
+
+import (
+	"testing"
+
+	"github.com/kioku-project/kioku/services/register/handler"
+
+	pblogin "github.com/kioku-project/kioku/services/login/proto"
+	"github.com/kioku-project/kioku/store"
+)
+
+func TestRegister(t *testing.T) {
+	var mockStore store.Store = nil
+	var mockLoginService pblogin.LoginService = nil
+
+	registerService := handler.New(mockStore, mockLoginService)
+
+	if registerService != nil {
+		t.Errorf("Received inbalid Register Service\n")
+	}
+}
